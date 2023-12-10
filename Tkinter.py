09013fff5df892e37702e514d79bd5563344b382
MarkoFletcher <smilenow170414@mail.ru>
2023-12-10 22:36:28 +0300

    Добавляю кнопки
    Пытаюсь осуществить проверку введенных данных

--- Tkinter.py
+++ Tkinter.py
@@ -1,4 +1,29 @@
-import tkinter
+from tkinter import *
+from tkinter import ttk
+
 import calculator
 
+window = Tk()
+window.title('Калькулятор вычисления выслуги лет')
+window.geometry('400x300')
 
+
+def receive_text():
+    result = entry.get()
+    try:
+        if result.isdigit():
+            print(result)
+            print('Done date')
+    except:
+        print('введеные неверные данные')
+
+
+
+entry = ttk.Entry()
+entry.pack()
+
+btn = ttk.Button(text='Выполнить', command=receive_text)
+btn.pack()
+
+
+window.mainloop()
