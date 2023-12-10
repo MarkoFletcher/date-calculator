e0ec93013a8730ee1d1abddb3e2367d8db9a5f7e
MarkoFletcher <smilenow170414@mail.ru>
2023-12-10 18:39:53 +0300

    Сделал расчет разности дат
    Сделал расчет разности дат, добавил функцию склонения результата. Создал файл для составление GUI приложения.

--- calculator.py
+++ calculator.py
@@ -1,36 +1,62 @@
-from datetime import datetime, date, timedelta, time
+from datetime import datetime, date
+
+# Входные данные:
 
 pattern = '%d.%m.%Y'
-date_start = date(1994, 6, 18)
+date_start = date(2000, 4, 17)
 date_start_month = date_start.month
 date_start_day = date_start.day
-#___________________________
-date_end = date(2023, 12, 21)
+
+date_end = date(2023, 3, 18)
 date_end_month = date_end.month
 date_end_day = date_end.day
 
 int_year_start_date = date_start.year
 int_year_end_date = date_end.year
+
 int_month_start = date_start.month
 int_month_end = date_end.month
 
-year_count = 0
-month_count = 0
 
-def diffrence(start, end, count):
-    while start < end:
-        count += 1
-        start += 1
-    return count
+def word_conversion(digit: int, word: list[str]) -> str:
+    if digit % 10 == 1 and digit % 100 != 11:
+        return f'{digit} {word[0]}'
+    elif digit % 10 in (2, 3, 4) and not digit % 100 in (12, 13, 14):
+        return f'{digit} {word[1]}'
+    else:
+        return f'{digit} {word[2]}'
 
-quantity_year = diffrence(int_year_start_date, int_year_end_date, year_count) #разность между дами в годах
-quantity_month = diffrence(int_month_start, int_month_end, month_count) #разность между датами в месецах
 
-data_current_year_month = date(int_year_start_date + quantity_year, date_start_month + quantity_month, date_start_day)
+if date_start_day <= date_end_day and date_start_month < date_end_month:
+    quantity_year = int_year_end_date - int_year_start_date
+    quantity_month = date_end_month - date_start_month
+    days_count = date_end_day - date_start_day
 
-def diffrence_year_day(data_current_year_month, date_end):
-    return date_end - data_current_year_month
+elif date_start_day > date_end_day and date_start_month < date_end_month:
+    quantity_year = int_year_end_date - int_year_start_date
+    quantity_month = date_end_month - date_start_month - 1  # разность между датами в месецах
+    data_current_year_month = date(int_year_start_date + quantity_year, date_start_month + quantity_month, date_start_day)  # текущая дата с месяцвми и годами
+    days_count = date_end - data_current_year_month
 
-print(f'Результат разности между {datetime.strftime(date_start, pattern)} и {datetime.strftime(date_end, pattern)}: {quantity_year} лет {quantity_month} месяцев {diffrence_year_day(data_current_year_month, date_end).days} дней')
+elif date_start_day <= date_end_day and date_start_month > date_end_month:
+    quantity_year = int_year_end_date - int_year_start_date - 1  # разность между дами в годах
+    quantity_month = 12 - date_start_month + date_end_month
+    days_count = date_end_day - date_start_day
+
+elif date_start_day > date_end_day and date_start_month > date_end_month:
+    quantity_year = int_year_end_date - int_year_start_date - 1  # разность между дами в годах
+    quantity_month = 12 - date_start_month + date_end_month - 1
+    data_current_year_month = date(int_year_end_date, date_end_month - 1, date_start_day)
+    days_count = date_end - data_current_year_month
+
+
+quantity_year_conversion = word_conversion(quantity_year, word=['год', 'года', 'лет'])
+quantity_month_conversion = word_conversion(quantity_month, word=['месяц', 'месяца', 'месяцев'])
+days_count_conversion = word_conversion(days_count, word=['день', 'дня', 'дней'])
+
+print(f'Результат разности между {datetime.strftime(date_start, pattern)} и {datetime.strftime(date_end, pattern)}: '
+      f'{quantity_year_conversion} {quantity_month_conversion} {days_count_conversion}')
+
+
 
 
